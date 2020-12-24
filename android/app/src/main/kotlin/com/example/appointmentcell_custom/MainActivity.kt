78f87f6b223eb9104bb597bdf646e17bc4702c9a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-24 16:50:38 +0530

    FLUT-3272 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/appointmentcell_custom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appointmentcell_custom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(Appointment_MonthCellBuilder());
+
+class Appointment_MonthCellBuilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SafeArea(
+          child: SfCalendar(
+            view: CalendarView.month,
+            monthCellBuilder: monthCellBuilder,
+            dataSource: _getCalendarDataSource(),
+          ),
+        )));
+  }
+
+  Widget monthCellBuilder(BuildContext context, MonthCellDetails details) {
+
+    if (details.appointments.isNotEmpty) {
+      Appointment appointment=details.appointments[0];
+      return Container(
+        color: appointment.color,
+        child: Text(appointment.subject),
+      );
+    }
+    return Container(
+      color: Colors.blueGrey,
+      child: Text(details.date.day.toString()),
+    );
+  }
+
+  _AppointmentDataSource _getCalendarDataSource() {
+    List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now(),
+      endTime: DateTime.now().add(Duration(hours: 1)),
+      subject: 'Meeting',
+      color: Colors.blue,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -5)),
+      endTime: DateTime.now().add(Duration(days: -5)),
+      subject: 'Planning',
+      color: Colors.deepOrangeAccent,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -9)),
+      endTime: DateTime.now().add(Duration(days: -9)),
+      subject: 'Consulting',
+      color: Colors.pink,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -15)),
+      endTime: DateTime.now().add(Duration(days: -15)),
+      subject: 'Retrospective',
+      color: Colors.purpleAccent,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: 15)),
+      endTime: DateTime.now().add(Duration(days:15)),
+      subject: 'Customer Meeting',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: 5)),
+      endTime: DateTime.now().add(Duration(days: 5)),
+      subject: 'Sprint Plan',
+      color: Colors.lightGreenAccent,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: 9)),
+      endTime: DateTime.now().add(Duration(days: 9)),
+      subject: 'Weekly Report',
+      color: Colors.lime,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: 3)),
+      endTime: DateTime.now().add(Duration(days: 3)),
+      subject: 'Meeting',
+      color: Colors.cyan,
+    ));
+
+    return _AppointmentDataSource(appointments);
+  }
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
